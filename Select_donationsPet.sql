465ab6979670145ab6a33b7741d1321c389967a7
Raissa Macedo <raissarlsantos@gmaol.comon>
2024-06-13 15:20:39 -0300

    criando select

--- Select_donationsPet.sql
+++ Select_donationsPet.sql
@@ -4,3 +4,21 @@ USE donationspet; -- ARQUIVO PARA CRIAÇÃO DE SELECT
 SELECT * FROM Income;
 SELECT * FROM Inventory;
 SELECT * FROM FinancialTransactions;
+
+-- SELECTS gráfico Raissa
+
+
+
+
+SELECT 
+    CASE 
+        WHEN age <= 1 THEN 'Filhote'
+        WHEN age <= 5 THEN 'Adulto Jovem'
+        WHEN age <= 10 THEN 'Adulto'
+        ELSE 'Idoso'
+    END AS faixa_etaria,
+    COUNT(*) AS quantidade,
+    COUNT(*) * 100.0 / (SELECT COUNT(*) FROM Adoptions) AS percentual
+FROM Animals
+JOIN Adoptions ON Animals.animal_id = Adoptions.animal_id
+GROUP BY faixa_etaria;
